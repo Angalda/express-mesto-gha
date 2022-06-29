514f1d7e42e63264cb5c4361c6128e4c40f9459e
Angalda <angalda@mail.ru>
2022-06-29 10:27:51 +0300

    fix: user id

--- controllers/users.js
+++ controllers/users.js
@@ -12,10 +12,10 @@ module.exports.getUserId = (req, res) => {
   User.findById(req.params.id)
 
     .then((user) => {
-      if (!user) {
+      /*if (!user) {
         res.status(404).send({ message: 'Не найдено' });
-        return;
-      }
+        //return;
+      }*/
       res.status(200).send({ data: user })
     })
     .catch((err) => {
