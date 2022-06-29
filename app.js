e482c9630bc45e71c2a1e228c8e3cdc2ae03b832
Angalda <angalda@mail.ru>
2022-06-29 10:18:58 +0300

    fix: error path

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.use((req, res, next) => {
+  res.status(404).send({ message: 'Не найдено' })
+});
+
 
 //app.use(express.static(path.join(__dirname, 'public')));
 
