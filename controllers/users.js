9e1c4178e76dd79c37a959d3e2980c25ead2bf3e
Angalda <angalda@mail.ru>
2022-07-10 13:50:45 +0300

    fix: get users

--- controllers/users.js
+++ controllers/users.js
@@ -26,7 +26,7 @@ module.exports.getUser = (req, res, next) => {
 // возвращает всех пользователей
 module.exports.getUsers = (req, res, next) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
+    .then((users) => res.send(users))
     .catch(next);
 };
 
