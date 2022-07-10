434a01bc6d6002d7696fa8865f3a9da015c1a4b3
Angalda <angalda@mail.ru>
2022-07-10 21:44:18 +0300

    fix: login auth

--- models/user.js
+++ models/user.js
@@ -44,8 +44,8 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+userSchema.statics.findUserByCredentials = function credentials(email, password) {
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
