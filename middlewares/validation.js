897d5d7518c1b9a420fcbb94eb917b52ab72aaee
Angalda <angalda@mail.ru>
2022-07-11 20:54:32 +0300

    fix: card joi

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -45,8 +45,8 @@ module.exports.validationGetUserId = celebrate({
 });
 
 module.exports.validationCreateCard = celebrate({
-  params: Joi.object().keys({
-    name: Joi.string().required().min(2),
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
     link: Joi.string().required(),
   }),
 });
