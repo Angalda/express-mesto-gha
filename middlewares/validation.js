66ead6b4d229040b1e1f23d227641e7398c16427
Angalda <angalda@mail.ru>
2022-07-11 21:05:51 +0300

    fix: card url

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -47,7 +47,7 @@ module.exports.validationGetUserId = celebrate({
 module.exports.validationCreateCard = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required(),
+    link: Joi.string().required().custom(validateUrl),
   }),
 });
 
